5623eb9e2734fd7cc92c6b9a55401d051c3fd656
arjunrk19 <92449366+arjunrk19@users.noreply.github.com>
2025-07-17 11:08:40 +0530

    Create not_gate.v

---
+++ not_gate.v
@@ -0,0 +1,21 @@
+//RTL CODE
+
+module not_gate(input a ,output y);
+assign y = ~a;
+endmodule
+
+//TEST BENCH
+
+module not_gate_tb;
+reg a;
+wire y;
+not_gate uut (.a(a),.y(y));
+initial begin
+a=0;
+#10;
+a=1;
+#10
+$display ("time=%t,a=%d,y=%d",$time,a,y);
+$finish;
+end 
+endmodule
