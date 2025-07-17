d930e312d93b35844e5a236101313743a14da40b
arjunrk19 <92449366+arjunrk19@users.noreply.github.com>
2025-07-17 13:30:54 +0530

    Create nand_gate.v

---
+++ nand_gate.v
@@ -0,0 +1,39 @@
+//RTL
+
+module nand_gate(input a,b,output y);
+  assign y = ~(a&b);
+endmodule
+
+//TB
+
+module nand_gate_tb;
+  reg a;
+  reg b;
+  wire y;
+  
+  nand_gate dut(a,b,y);
+  
+  initial begin 
+    $monitor("The value of a=%d,b=%d,y=%d",a,b,y);
+    a=1'b0;
+    b=1'b0;
+    #10;
+    
+    a=1'b0;
+    b=1'b1;
+    #10;
+    
+    a=1'b1;
+    b=1'b0;
+    #10;
+   
+     
+    a=1'b1;
+    b=1'b1;
+    #10;
+   
+   
+    
+    $finish;
+  end
+  endmodule
